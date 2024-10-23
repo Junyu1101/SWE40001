029749175b06ff0408c760c9a231acb55a852fef
Junyu1101 <82603394+Junyu1101@users.noreply.github.com>
2024-10-23 17:10:24 +0800

    Add files via upload
    db.php - Connection for database
    fetch_promotion.php - Fetch promotions list from the database
    fetch_promotion_detail.php - Fetch promotions details from the database
    promotion/style.css - My style.css that use in my promotion page
    promotion.php - Display List of promotions in the database
    promotion_details.php - Display promotions details based on what promotion is clicked
    
    This is not the final version of the promotion page yet. Will have more updates on promotions page

---
+++ promotion/fetch_promotion_detail.php
@@ -0,0 +1,19 @@
+<?php
+include 'db.php';
+
+$promotionID = isset($_GET['id']) ? intval($_GET['id']) : 0;
+
+$sql = "SELECT promotion.title, promotion.description, promotion.start_date, promotion.end_date, promotion.code , product.name 
+        FROM promotion 
+        JOIN product ON promotion.productID = product.productID 
+        WHERE promotion.promotionID = $promotionID";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $promotion = $result->fetch_assoc();
+    echo json_encode($promotion);
+} else {
+    echo json_encode(['error' => 'Promotion not found']);
+}
+?>
