3948f41c00b9e7e3aafa7fa11ed155068d8e74d6
Aynsley-A <101215711@students.swinburne.edu.my>
2024-10-25 19:42:35 +0800

    Adding new product details page and add styles in css file

---
+++ product-details.php
@@ -0,0 +1,20 @@
+<?php include 'header.php'; ?>
+   
+
+    <div class="product-details-container">
+        <!-- Product Image -->
+        <div class="product-image">
+            <img src="productIMG/sweetcorn.jpeg" alt="Product 1">
+        </div>
+
+        <!-- Product Information -->
+        <div class="product-info">
+            <h1>CCK SWEET CORN 500G</h1>
+            <p>One of the main nutritional benefits of sweetcorn is its high fibre content. And as we know, dietary fibre is important for our health: it aids digestion, it can decrease the risk of heart disease, strokes.</p>
+            <p>On top of that, fibre helps you stay fuller for longer just like consuming potatoes.</p>
+            <div class="product-price">RM4.90 ~ RM6.20</div>
+        </div>
+    </div>
+
+<?php include 'footer.php'; ?>
+
